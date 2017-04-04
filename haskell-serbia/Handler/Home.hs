8c6d6bb2a0eea29be659395fd8d66b17186a0679
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-04-04 11:08:00 +0200

    Get the login form to type-check

--- haskell-serbia/Handler/Home.hs
+++ haskell-serbia/Handler/Home.hs
@@ -1,15 +1,16 @@
 module Handler.Home where
 
 import Import
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
-import Text.Julius (RawJS (..))
+import Yesod.Form.Bootstrap3
+       (BootstrapFormLayout(..), renderBootstrap3)
+import Text.Julius (RawJS(..))
 import Widget.Header
 
 -- Define our data that will be used for creating the form.
 data FileForm = FileForm
-    { fileInfo :: FileInfo
-    , fileDescription :: Text
-    }
+  { fileInfo :: FileInfo
+  , fileDescription :: Text
+  }
 
 twitterUsername :: String
 twitterUsername = "haskellserbia"
@@ -23,45 +24,45 @@ twitterUsername = "haskellserbia"
 -- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe FileForm
-        handlerName = "getHomeR" :: Text
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Haskell Srbija"
-        twitterWidget twitterUsername
-          $(widgetFile "homepage")
+  (formWidget, formEnctype) <- generateFormPost sampleForm
+  let submission = Nothing :: Maybe FileForm
+      handlerName = "getHomeR" :: Text
+  defaultLayout $
+    do let (commentFormId, commentTextareaId, commentListId) = commentIds
+       aDomId <- newIdent
+       setTitle "Haskell Srbija"
+       twitterWidget twitterUsername
+       $(widgetFile "homepage")
 
 postHomeR :: Handler Html
 postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
-
-    defaultLayout $ do
-        let (commentFormId, commentTextareaId, commentListId) = commentIds
-        aDomId <- newIdent
-        setTitle "Haskell Srbija"
-        $(widgetFile "homepage")
+  ((result, formWidget), formEnctype) <- runFormPost sampleForm
+  let handlerName = "postHomeR" :: Text
+      submission =
+        case result of
+          FormSuccess res -> Just res
+          _ -> Nothing
+  defaultLayout $
+    do let (commentFormId, commentTextareaId, commentListId) = commentIds
+       aDomId <- newIdent
+       setTitle "Haskell Srbija"
+       $(widgetFile "homepage")
 
 sampleForm :: Form FileForm
-sampleForm = renderBootstrap3 BootstrapBasicForm $ FileForm
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField textSettings Nothing
-    -- Add attributes like the placeholder and CSS classes.
-    where textSettings = FieldSettings
-            { fsLabel = "What's on the file?"
-            , fsTooltip = Nothing
-            , fsId = Nothing
-            , fsName = Nothing
-            , fsAttrs =
-                [ ("class", "form-control")
-                , ("placeholder", "File description")
-                ]
-            }
+sampleForm =
+  renderBootstrap3 BootstrapBasicForm $
+  FileForm <$> fileAFormReq "Choose a file" <*> areq textField textSettings Nothing
+-- Add attributes like the placeholder and CSS classes.
+  where
+    textSettings =
+      FieldSettings
+      { fsLabel = "What's on the file?"
+      , fsTooltip = Nothing
+      , fsId = Nothing
+      , fsName = Nothing
+      , fsAttrs =
+        [("class", "form-control"), ("placeholder", "File description")]
+      }
 
 commentIds :: (Text, Text, Text)
 commentIds = ("js-commentForm", "js-createCommentTextarea", "js-commentList")
--- haskell-serbia/Handler/UserLogin.hs
+++ haskell-serbia/Handler/UserLogin.hs
@@ -4,9 +4,8 @@ import Import
 import Yesod.Form.Bootstrap3
 import Yesod.Form.Jquery
 
-  
 -- The datatype we wish to receive from the form
-data Person = Person
+data UserLogin = UserLogin
     { personName          :: Text
     , personBirthday      :: Day
     , personFavoriteColor :: Maybe Text
@@ -16,8 +15,8 @@ data Person = Person
   deriving Show
 
 
-personForm :: Html -> MForm Handler (FormResult Person, Widget)
-personForm = renderDivs $ Person
+userLoginForm :: Html -> MForm Handler (FormResult UserLogin, Widget)
+userLoginForm = renderDivs $ UserLogin
     <$> areq textField "Name" Nothing
     <*> areq (jqueryDayField def
         { jdsChangeYear = True -- give a year dropdown
@@ -31,13 +30,12 @@ personForm = renderDivs $ Person
 getUserLoginR :: Handler Html
 getUserLoginR = do
 -- Generate the form to be displayed
-    (widget, enctype) <- generateFormPost personForm
+    (widget, enctype) <- generateFormPost userLoginForm
     defaultLayout
-        [whamlet|
-            <p>
+        [whamlet| <p>
                 The widget generated contains only the contents
                 of the form, not the form tag itself. So...
-            <form method=post action=@{PersonR} enctype=#{enctype}>
+            <form method=post action=@{UserLoginR} enctype=#{enctype}>
                 ^{widget}
                 <p>It also doesn't include the submit button.
                 <button>Submit
@@ -45,13 +43,12 @@ getUserLoginR = do
 
 postUserLoginR :: Handler Html
 postUserLoginR = do
-  ((result, widget), enctype) <- runFormPost personForm
+  ((result, widget), enctype) <- runFormPost userLoginForm
   case result of
         FormSuccess person -> defaultLayout [whamlet|<p>#{show person}|]
         _ -> defaultLayout
-            [whamlet|
-                <p>Invalid input, let's try again.
-                <form method=post action=@{PersonR} enctype=#{enctype}>
+            [whamlet| <p>Invalid input, let's try again.
+                <form method=post action=@{UserLoginR} enctype=#{enctype}>
                     ^{widget}
                     <button>Submit
             |]
