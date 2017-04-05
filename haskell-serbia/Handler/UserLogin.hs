b0c54d1b807d77f8556b67b2043660f14bda4db7
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-04-05 23:18:12 +0200

    Add form templates
    TO-DO fetch post params and create data type

--- haskell-serbia/Handler/UserLogin.hs
+++ haskell-serbia/Handler/UserLogin.hs
@@ -3,47 +3,23 @@ module Handler.UserLogin where
 import Import
 import Yesod.Form.Bootstrap3
 import Yesod.Form.Jquery
+import Data.Maybe
 
 -- The datatype we wish to receive from the form
 data UserLogin = UserLogin
-    { personName          :: Text
-    , personEmail         :: Text
-    }
-  deriving Show
-
-
-
-userLoginForm :: Html -> MForm Handler (FormResult UserLogin, Widget)
-userLoginForm = renderBootstrap3 BootstrapBasicForm $ UserLogin
-    <$> areq textField "Name" (Just "Your name")
-    <*> areq emailField "Email address" (Just "email")
-
+  { personName :: Text
+  , personEmail :: Text
+  } deriving (Show)
 
 getUserLoginR :: Handler Html
 getUserLoginR = do
-    (widget, enctype) <- generateFormPost userLoginForm
-    defaultLayout
-        [whamlet| <div .row col-md-6 .col-md-offset-2>
-                    <form method=post action=@{UserLoginR} enctype=#{enctype}>
-                      ^{widget}
-                      <button .btn .btn-default .btn-xs>Create account
-        |]
+  defaultLayout $ do $(widgetFile "forms/userlogin")
 
 postUserLoginR :: Handler Html
 postUserLoginR = do
-  ((result, widget), enctype) <- runFormPost userLoginForm
-  case result of
-        FormSuccess person -> defaultLayout [whamlet|<div .row col-md-6 .col-md-offset-2>
-                                                      <p>You submited the request for new account. <br />Your details
-                                                      <p> #{personName person}
-                                                      <p> #{personEmail person}
-                                                    |]
-        _ -> defaultLayout
-            [whamlet| <div .row col-md-6 col-md-offset-2>
-                    <p>Invalid input, you can try again.
-                <form method=post action=@{UserLoginR} enctype=#{enctype}>
-                    ^{widget}
-                    <button>Create account
-            |]
-
-
+  name <- lookupPostParam "user_name"
+  email <- lookupPostParam "user_email"
+  let user = UserLogin name email
+  if user
+    then defaultLayout $ do $(widgetFile "forms/userloginsuccess")
+    else defaultLayout $ do $(widgetFile "forms/userloginerror")
--- haskell-serbia/Widget/Header.hs
+++ haskell-serbia/Widget/Header.hs
@@ -5,6 +5,7 @@ import Import
 -- twitter widget to render follow button
 twitterWidget :: String -> WidgetT App IO ()
 twitterWidget twitteruser = do
-   toWidgetBody([hamlet| <a href="https://twitter.com/#{twitteruser}" .twitter-follow-button data-show-count="true">Follow @#{twitteruser}
-                         <script async src="//platform.twitter.com/widgets.js" charset="utf-8">
+   toWidgetBody([hamlet|<div style=margin-top:20px>
+                          <a href="https://twitter.com/#{twitteruser}" .twitter-follow-button data-show-count="true">Follow @#{twitteruser}
+                          <script async src="//platform.twitter.com/widgets.js" charset="utf-8">
                        |])
