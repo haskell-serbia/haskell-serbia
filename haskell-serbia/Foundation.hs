2b996695223bf050c4855d68cbb57e5940417bb4
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-04-05 16:59:42 +0200

    Clean up home handler

--- haskell-serbia/Foundation.hs
+++ haskell-serbia/Foundation.hs
@@ -138,8 +138,7 @@ instance Yesod App where
     isAuthorized CommentR _ = return Authorized
     isAuthorized HomeR _ = return Authorized
     isAuthorized UserLoginR _ = return Authorized
-
-
+    isAuthorized (BlogR _) _   = return Authorized
     isAuthorized FaviconR _ = return Authorized
     isAuthorized RobotsR _ = return Authorized
     isAuthorized (StaticR _) _ = return Authorized
