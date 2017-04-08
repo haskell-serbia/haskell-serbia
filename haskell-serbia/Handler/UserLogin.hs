1aafaf14e21c7567ccbcc444e8a207ceb5b31195
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-04-08 22:31:31 +0200

    Switch to monad form

--- haskell-serbia/Handler/UserLogin.hs
+++ haskell-serbia/Handler/UserLogin.hs
@@ -4,25 +4,66 @@ import Import
 
 -- The datatype we wish to receive from the form
 data UserLogin = UserLogin
-  { personName :: Text
-  , personEmail :: Text
+  { userName :: Text
+  , userEmail :: Text
   } deriving (Show)
+
+-- fromString "nameForm" = FieldSettings{
+--                     fsLabel = "User name"
+--                    , fsTooltip = "User name"
+--                    , fsId =  Nothing
+--                    , fsName = Nothing
+--                    , fsAttrs = []
+--                  }
+-- fromString "emailForm" = FieldSettings{
+--                     fsLabel = "Email"
+--                    , fsTooltip = "email"
+--                    , fsId =  Nothing
+--                    , fsName = Nothing
+--                    , fsAttrs = []
+--                  }
+
+userLoginForm :: Html -> MForm Handler (FormResult UserLogin, Widget)
+userLoginForm = do
+    (nameRes, nameView) <- mreq textField "name Field" Nothing
+    (emailRes, emailForm) <- mreq emailField "email Field" Nothing
+    let userLoginRes = UserLogin <$> nameRes <*> emailRes
+    let widget = do
+            [whamlet|
+              <div .row col-md-6 .col-md-offset-2>
+                      ^{fvInput nameView}
+                      ^{fvInput emailView}
+                       <button .btn .btn-default .btn-xs type=submit>Submit
+            |]
+    return (userLoginRes, widget)
+
+
+
 
 getUserLoginR :: Handler Html
 getUserLoginR = do
-  defaultLayout $ do $(widgetFile "forms/userlogin")
-
-userLoginForm :: Form UserLogin
-userLoginForm = renderDivs $ UserLogin
-    <$> areq textField "Name" Nothing
-    <*> areq emailField "Email" Nothing
+    (widget, enctype) <- runFormGet userLoginForm
+    defaultLayout
+      [whamlet|
+              <form method=post action=@{UserLoginR} enctype=#{enctype}>
+                  ^{widget}
+      |]
 
 postUserLoginR :: Handler Html
 postUserLoginR = do
-  ((result, _), _) <- runFormPost $ userLoginForm
-  defaultLayout $ do
-  case result of
-    FormSuccess user  -> do
-        $(widgetFile "forms/userloginsuccess")
-    _  -> do
-        $(widgetFile "forms/userloginerror")
+    ((result, widget), enctype) <- runFormPost userLoginForm
+    case result of
+        FormSuccess person -> defaultLayout
+              [whamlet|
+                    <div .row col-md-6 .col-md-offset-2>
+                        <p>#{show person}
+                  |]
+        _ -> defaultLayout
+              [whamlet|
+                  <div .row col-md-6 .col-md-offset-2>
+                    <p>Invalid input, let's try again.
+                    <form method=post action=@{UserLoginR} enctype=#{enctype}>
+                        ^{widget}
+                        <button .btn .btn-default .btn-xs type=submit>Submit
+              |]
+
