4949b6b815357f52ea89190c272fa578e9a37c8e
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-04-10 23:42:34 +0200

    Add esqueleto and do select to get email

--- haskell-serbia/Handler/UserLogin.hs
+++ haskell-serbia/Handler/UserLogin.hs
@@ -2,6 +2,9 @@ module Handler.UserLogin where
 
 import Import
 import Yesod.Form.Bootstrap3
+import qualified Database.Esqueleto as E
+import Database.Esqueleto ((^.), select, from, where_, val)
+import Data.Maybe
 
 -- The datatype we wish to receive from the form
 data UserLogin = UserLogin
@@ -10,46 +13,55 @@ data UserLogin = UserLogin
   } deriving (Show)
 
 userLoginForm :: Html -> MForm Handler (FormResult UserLogin, Widget)
-userLoginForm = renderDivs $ UserLogin
-    <$> areq textField (bfs ("User name" :: Text)) Nothing
-    <*> areq emailField (bfs ("Email address" :: Text)) Nothing
-
-
+userLoginForm =
+  renderDivs $
+  UserLogin <$> areq textField (bfs ("User name" :: Text)) Nothing <*>
+  areq emailField (bfs ("Email address" :: Text)) Nothing
 
 getUserLoginR :: Handler Html
 getUserLoginR = do
-    (widget, enctype) <- generateFormPost userLoginForm
-    defaultLayout
-        [whamlet|
+  (widget, enctype) <- generateFormPost userLoginForm
+  defaultLayout
+    [whamlet|
             <div .col-md-6 .offset-md-2>
               <form method=post action=@{UserLoginR} enctype=#{enctype}>
                   ^{widget}
                   <button .btn .btn-default>Submit
         |]
 
+checkEmail :: Text -> HandlerT App IO [Entity Email]
+checkEmail email = do
+  runDB $
+    E.select $
+    E.from $
+    \e -> do
+      E.where_ (e E.^. EmailEmail E.==. E.val email)
+      E.limit 1
+      return e
+
 postUserLoginR :: Handler Html
 postUserLoginR = do
-    ((result, widget), enctype) <- runFormPost userLoginForm
-    case result of
-        FormSuccess user -> defaultLayout $ do
-          dbuser <- selectFirst [Email =. userEmail user] []
-          case dbuser of
-            Nothing ->
-                    [whamlet|
-                          <div .col-md-6 .offset-md-2>
-                            <p>#{userName user}
-                            <p>#{userEmail user}
-
-                    |]
-            _ ->
-                    [whamlet|
-                          <div .col-md-6 .offset-md-2>
-                            <p>User with the same email is already registered!
-
-                    |]
-
-        _ -> defaultLayout
-              [whamlet|
+  ((result, widget), enctype) <- runFormPost userLoginForm
+  case result of
+    FormSuccess user -> do
+      emailExists <- checkEmail $ userEmail user
+      case emailExists of
+        null ->
+          defaultLayout
+            [whamlet|
+                              <div .col-md-6 .offset-md-2>
+                                <p>#{userName user}
+                                <p>#{userEmail user}
+                        |]
+        _ ->
+          defaultLayout
+            [whamlet|
+                              <div .col-md-6 .offset-md-2>
+                                <p>That email is already registered
+                        |]
+    _ ->
+      defaultLayout
+        [whamlet|
                  <div .col-md-6 .offset-md-2>
                     <p>Invalid input, let's try again.
                     <form method=post action=@{UserLoginR} enctype=#{enctype}>
