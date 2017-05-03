1e7a657c71e38aa874eafb4655cf09bc61df1052
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-05-03 10:58:47 +0200

    Add created by to tutorial

--- haskell-serbia/Handler/TutorialR.hs
+++ haskell-serbia/Handler/TutorialR.hs
@@ -7,5 +7,7 @@ getTutorialRR :: TutorialId -> Handler Html
 getTutorialRR tutorialId = do
   muid <- fmap entityKey <$> maybeAuth
   tut <- runDB $ get404 tutorialId
+  author <- runDB $ get404 $ tutorialCreatedBy tut
+
   defaultLayout $ do
     $(widgetFile "tutorials/tut")
