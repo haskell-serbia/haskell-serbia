0d5e19533412b6845c5bbcff85021d77adfedf07
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-05-07 13:42:51 +0200

    Remove quotes

--- haskell-serbia/Widget/Disqus.hs
+++ haskell-serbia/Widget/Disqus.hs
@@ -8,16 +8,16 @@ disqusWidget articleIdentifier = do
    let page_url = String "http://haskell-serbia.com/tutorial/"
    let jsLink  = String "https://haskell-serbia.disqus.com/embed.js"
    toWidget([hamlet|
-                <div #disqus_thread> 
+                <div #disqus_thread>
         |])
    toWidget([julius|
-                (function() { 
+                (function() {
                     var disqus_config = function () {
-                        this.page.url = #{page_url}; 
-                        this.page.identifier = #{textId};
+                        this.page.url = '#{page_url}';
+                        this.page.identifier = '#{textId}';
                     };
                     var d = document, s = d.createElement('script');
-                    s.src = '#{jsLink}';
+                    s.src = #{jsLink};
                     s.setAttribute('data-timestamp', +new Date());
                     (d.head || d.body).appendChild(s);
                 })();
