d1cdf5024daab329dd7c8eaf9b1e377d72aae125
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-06-11 22:40:11 +0200

    Add some pagination code

--- haskell-serbia/Handler/TutorialList.hs
+++ haskell-serbia/Handler/TutorialList.hs
@@ -5,6 +5,9 @@ import Database.Esqueleto as E
 import Text.Read (read, readMaybe)
 import Import
 
+postsByPage :: Int
+postsByPage = 5
+
 selectCount q = do
   res <- select $ from $ (\x -> q x >> return countRows)
   return $ fromMaybe 0 $ (\(Value a) -> a) <$> headMay res
@@ -15,17 +18,21 @@ getTutorialListR currentPage = do
   entriesCount <- runDB $ selectCount $ \tutorial -> do
                   E.where_  (tutorial ^. TutorialCreatedAt E.<=. E.val now)
 
-  let next = calculateNextPage entriesCount 1 currentPage
-  let previous = calculatePreviousPage entriesCount 1 currentPage
-  allPosts <- runDB $ selectList [] [Desc TutorialId, LimitTo 5, OffsetBy (currentPage - 1)]
+  let next = calculateNextPage entriesCount postsByPage currentPage
+  let previous = calculatePreviousPage entriesCount postsByPage currentPage
+  let off = if (currentPage - postsByPage) < 0 then 0 else (currentPage - postsByPage)
+  allPosts <- runDB $ selectList [] [Desc TutorialId, LimitTo postsByPage, OffsetBy off]
 
   defaultLayout $ do
     $(widgetFile "tutorials/all")
 
 calculatePreviousPage :: Int -> Int -> Page -> Maybe Int
 calculatePreviousPage entries pageSize currentPage =
-  if currentPage == 2 then Just 1 else Nothing
+  if n > 0 then Just n else Nothing
+  where n = if (entries - (pageSize * currentPage) - pageSize) > 0 then (entries - (pageSize * currentPage) - pageSize) else 0
 
 calculateNextPage :: Int -> Int -> Int -> Maybe Int
 calculateNextPage entries pageSize currentPage =
-  if currentPage == 1 then Just 2 else Nothing
+  if n <= entries  then Just n else Nothing
+  where n =  entries - (pageSize * currentPage) + pageSize
+
