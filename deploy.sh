91d101363b64637d3ed23773134004f8f98ad3e0
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-08-28 14:26:08 +0200

    Remove sudo from deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
-scp -i  ~/Documents/haskell-serbia/haskell-serbia.pem.txt ~/code/haskell-serbia/bin/haskell-serbia  ubuntu@ec2-34-195-52-222.compute-1.amazonaws.com:/home/ubuntu
+# scp -i  ~/Documents/haskell-serbia/haskell-serbia.pem.txt ~/code/haskell-serbia/bin/haskell-serbia  ubuntu@ec2-34-195-52-222.compute-1.amazonaws.com:/home/ubuntu
 ssh -i ~/Documents/haskell-serbia/haskell-serbia.pem.txt  ubuntu@ec2-34-195-52-222.compute-1.amazonaws.com 'bash -s' < install.sh
