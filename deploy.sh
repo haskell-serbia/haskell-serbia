25b6e39d068c238a4a2c4ea5789ed44572c1c30e
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-08-28 09:41:05 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+scp -i  ~/Documents/haskell-serbia/haskell-serbia.pem.txt ~/code/haskell-serbia/bin/haskell-serbia  ubuntu@ec2-34-195-52-222.compute-1.amazonaws.com:/home/ubuntu
+ssh -i ~/Documents/haskell-serbia/haskell-serbia.pem.txt  ubuntu@ec2-34-195-52-222.compute-1.amazonaws.com 'bash -s' < install.sh
