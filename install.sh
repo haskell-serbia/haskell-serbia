1e76e93fffe6befdda7c769c9f38dc2722a6c6de
Sasa Bogicevic <bogicevicsasa@yahoo.com>
2017-08-28 20:53:55 +0200

    Add db copy command to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ sudo chmod a+x haskell-serbia
 sudo mv haskell-serbia /var/www/html/haskell-serbia/haskell-serbia_bin
 ps -ef | grep haskell | grep -v grep | awk '{print $2}' | xargs kill -9
 cd /var/www/html/haskell-serbia/
+sudo cp haskell-serbia-sqlite.sqlite3 /var/www/html
 sudo git stash
 sudo git pull origin master
 nohup sudo ./haskell-serbia_bin &
